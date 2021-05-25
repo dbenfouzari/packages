614dd4b6e194b2168af66b2a4f4c3f8b57ea772b
Donovan BENFOUZARI <d.benfouzari@gmail.com>
2021-05-25 14:55:20 +0200

    :bookmark: Publish v5.0.2

---
+++ packages/react-i18n/src/hooks/useI18n.ts
@@ -0,0 +1,11 @@
+import I18n from "@dbenfouzari/i18n";
+import { useContext } from "react";
+import { I18nContext } from "../i18n-provider";
+
+const useI18n = () => {
+  const i18n = useContext(I18nContext);
+
+
+};
+
+export default useI18n;
---
+++ packages/react-i18n/src/i18n-provider.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import I18n from "@dbenfouzari/i18n";
+
+export const I18nContext = React.createContext(null);
+
+export default I18nContext.Provider;
